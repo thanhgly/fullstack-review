936512ba0b7bd006d8a8aaab6ccad86336c638e4
Thanh Ly <thanhgly.se@gmail.com>
2022-12-07 17:18:04 -0800

    Complete save function for database

--- code.test.js
+++ code.test.js
@@ -1,14 +1,54 @@
 const axios = require('axios');
+const mongoose = require('mongoose');
 const config = require('./config.js');
-const getReposByUsername = require('./helpers/github.js').getReposByUsername;
-
+const Helpers = require('./helpers/github.js');
+const db = require('./database/index.js');
 
 describe("getReposByUsername", function() {
-  it("should eventually resolve with repos from github API", function() {
-    getReposByUsername('thanhgly', (err, data) => {
+  it("should eventually resolve with repos from github API", function(done) {
+    Helpers.getReposByUsername('thanhgly', (err, data) => {
       if (err) throw err;
-      expect(data).toBeDefined();
+      var repo = data[0];
+      expect(repo.full_name).toEqual('thanhgly/recursion-prompts');
+      done();
     });
 
   });
 });
+
+describe("save", function() {
+
+  beforeEach((done) => {
+    db.Repo.collection.drop();
+    done();
+  });
+
+  afterAll(() => {
+    mongoose.disconnect('mongodb://localhost/fetcher')
+  });
+
+  it('should save repos from github API to database', function(done) {
+    axios.get('https://api.github.com/users/thanhgly/repos')
+      .then((result) => {
+        return result.data;
+      })
+      .then((repos) => {
+        return db.save(repos)
+      })
+      .then((savedData) => {
+        return db.Repo.find()
+      })
+      .then((data) => {
+        var repo = data[0];
+        expect(repo._id).toEqual(489763231);
+        expect(repo.name).toEqual('recursion-prompts');
+        expect(repo.owner).toEqual('thanhgly');
+        expect(repo.link).toEqual('https://github.com/thanhgly/recursion-prompts');
+        done();
+      })
+      .catch((err) => {
+        console.log('ERROR', err);
+        done();
+      })
+  })
+})
--- database/index.js
+++ database/index.js
@@ -2,15 +2,30 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  _id: Number,
+  name: String,
+  owner: String,
+  link: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repos) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  var newRepos = repos.map((repo) => {
+    var newRepo = new Repo({
+      _id: repo.id,
+      name: repo.name,
+      owner: repo.owner.login,
+      link: repo.html_url
+    });
+    return newRepo.save();
+  });
+  return Promise.all(newRepos);
 }
 
+module.exports.Repo = Repo;
 module.exports.save = save;
